650c9163fa30ea59ab16fee4c3c5f991f9e0d31c
sitiporn1 <sit.mte@gmail.com>
2021-10-08 20:07:09 +0700

    edit load intent class

--- implement_papers/utils.py
+++ implement_papers/utils.py
@@ -11,15 +11,16 @@ class IntentExample:
         if do_lower_case:
             self.text = self.text.lower()
         
-def load_intent_examples(file_path, do_lower_case=True):
-    examples = []
+    def load_intent_examples(file_path, do_lower_case=True):
+       
+        examples = []
 
-    with open('{}/seq.in'.format(file_path), 'r', encoding="utf-8") as f_text, open('{}/label'.format(file_path), 'r', encoding="utf-8") as f_label:
-        for text, label in zip(f_text, f_label):
-            e = IntentExample(text.strip(), label.strip(), do_lower_case)
-            examples.append(e)
+        with open('{}/seq.in'.format(file_path), 'r', encoding="utf-8") as f_text, open('{}/label'.format(file_path), 'r', encoding="utf-8") as f_label:
+            for text, label in zip(f_text, f_label):
+                e = IntentExample(text.strip(), label.strip(), do_lower_case)
+                examples.append(e)
 
-    return examples
+        return examples
 
 class loss(object):
 
