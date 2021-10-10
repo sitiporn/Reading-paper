328b3eb779f718b0e95ef83008beb4b44fc4ef50
sitiporn1 <sit.mte@gmail.com>
2021-10-10 23:00:18 +0700

    add detail dropout encoder and move function to under class

--- implement_papers/dataloader.py
+++ implement_papers/dataloader.py
@@ -1,3 +1,4 @@
+from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
 import random
 
 class IntentExample(object):
@@ -58,3 +59,59 @@ def sample(N, examples):
         sampled_examples.append({'task': l, 'examples': examples})
 
     return sampled_examples
+
+class Classifier:
+    def __init__(self,path: str,label_list,args):
+
+        self.label_list = label_list
+        self.num_labels = len(self.label_list)
+
+    def convert_examples_to_features(examples, train):
+            label_map = {label: i for i, label in enumerate(self.label_list)}
+            if_roberta = True if "roberta" in self.config.architectures[0].lower() else False
+            
+            if train:
+                label_distribution = torch.FloatTensor(len(label_map)).zero_()
+            else:
+                label_distribution = None
+
+            features = []
+            for (ex_index, example) in enumerate(examples):
+                tokens_a = self.tokenizer.tokenize(example.text_a)
+
+                if len(tokens_a) > self.args.max_seq_length - 2:
+                    tokens_a = tokens_a[:(self.args.max_seq_length - 2)]
+
+                tokens = [self.tokenizer.cls_token] + tokens_a + [self.tokenizer.sep_token]
+                segment_ids = [0] * len(tokens)
+                input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+                input_mask = [1] * len(input_ids)
+                padding = [0] * (self.args.max_seq_length - len(input_ids))
+
+                input_ids += padding
+                input_mask += padding
+                segment_ids += padding
+
+                assert len(input_ids) == self.args.max_seq_length
+                assert len(input_mask) == self.args.max_seq_length
+                assert len(segment_ids) == self.args.max_seq_length
+
+                if example.label is not None:
+                    label_id = label_map[example.label]
+                else:
+                    label_id = -1
+
+                if train:
+                    label_distribution[label_id] += 1.0
+                    
+                features.append(
+                    InputFeatures(input_ids=input_ids,
+                                  input_mask=input_mask,
+                                  segment_ids=segment_ids,
+                                  label_id=label_id))
+                
+            if train:
+                label_distribution = label_distribution / label_distribution.sum()
+                return features, label_distribution
+            else:
+                return features
