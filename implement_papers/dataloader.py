c8a1605745c5bae2522d89ba7a319163754f6baf
sitiporn1 <sit.mte@gmail.com>
2021-10-15 23:57:26 +0700

    add encode

--- implement_papers/dataloader.py
+++ implement_papers/dataloader.py
@@ -60,58 +60,33 @@ def sample(N, examples):
 
     return sampled_examples
 
-class Classifier:
-    def __init__(self,path: str,label_list,args):
 
-        self.label_list = label_list
-        self.num_labels = len(self.label_list)
+class Similarity(nn.Module):
+    """
+    Dot product or cosine similarity
+    """
 
-    def convert_examples_to_features(examples, train):
-            label_map = {label: i for i, label in enumerate(self.label_list)}
-            if_roberta = True if "roberta" in self.config.architectures[0].lower() else False
-            
-            if train:
-                label_distribution = torch.FloatTensor(len(label_map)).zero_()
-            else:
-                label_distribution = None
+    def __init__(self, temp):
+        super().__init__()
+        self.temp = temp
+        self.cos = nn.CosineSimilarity(dim=-1)
 
-            features = []
-            for (ex_index, example) in enumerate(examples):
-                tokens_a = self.tokenizer.tokenize(example.text_a)
+    def forward(self, x, y):
+        return self.cos(x, y) / self.temp
 
-                if len(tokens_a) > self.args.max_seq_length - 2:
-                    tokens_a = tokens_a[:(self.args.max_seq_length - 2)]
+class SimCSE(object):
+    """
+    class for embeddings sentence by using BERT 
 
-                tokens = [self.tokenizer.cls_token] + tokens_a + [self.tokenizer.sep_token]
-                segment_ids = [0] * len(tokens)
-                input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
-                input_mask = [1] * len(input_ids)
-                padding = [0] * (self.args.max_seq_length - len(input_ids))
+    """
+    def __init__(self):
+        
+        self.tokenizer = None
+        self.model = None
+        self.device = device
 
-                input_ids += padding
-                input_mask += padding
-                segment_ids += padding
+    def encode(self,sentence: Union[str, List[str]],batch_size : int = 64):
+        
 
-                assert len(input_ids) == self.args.max_seq_length
-                assert len(input_mask) == self.args.max_seq_length
-                assert len(segment_ids) == self.args.max_seq_length
+        self.model = self.model.to(target_device)
 
-                if example.label is not None:
-                    label_id = label_map[example.label]
-                else:
-                    label_id = -1
-
-                if train:
-                    label_distribution[label_id] += 1.0
-                    
-                features.append(
-                    InputFeatures(input_ids=input_ids,
-                                  input_mask=input_mask,
-                                  segment_ids=segment_ids,
-                                  label_id=label_id))
-                
-            if train:
-                label_distribution = label_distribution / label_distribution.sum()
-                return features, label_distribution
-            else:
-                return features
