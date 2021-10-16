46ac25ec615e8d4637d3b9e6ea070297ba785d2a
sitiporn1 <sit.mte@gmail.com>
2021-10-16 15:59:49 +0700

    get hidden representation of single sentence

--- implement_papers/dataloader.py
+++ implement_papers/dataloader.py
@@ -1,5 +1,11 @@
+import torch
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from transformers import AutoModel, AutoTokenizer
 import random
+import torch.nn as nn
+import torch.nn.functional as F
+from typing import List, Dict, Tuple, Type, Union
 
 class IntentExample(object):
 
@@ -79,14 +85,45 @@ class SimCSE(object):
     class for embeddings sentence by using BERT 
 
     """
-    def __init__(self):
-        
-        self.tokenizer = None
-        self.model = None
+    def __init__(self,device):
+        self.model = AutoModel.from_pretrained('bert-base-uncased')
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
         self.device = device
 
-    def encode(self,sentence: Union[str, List[str]],batch_size : int = 64):
+    def encode(self,sentence:Union[str, List[str]],batch_size : int = 64,max_length:int = 128):
         
 
+        target_device = self.device 
         self.model = self.model.to(target_device)
+        
+        if isinstance(sentence,str):
+            sentence = [sentence]
+            single_sentence = True 
 
+
+        embedding_list = []
+
+        with torch.no_grad():
+            total_batch = len(sentence) // batch_size + (1 if len(sentence) % batch_size > 0 else 0)
+
+            """
+            print(total_batch)
+            print(sentence)
+            print(type(sentence))
+            print(dir(self.tokenizer))
+            """
+            assert str == type(sentence[0])
+            inputs = self.tokenizer(sentence[0],padding=True,truncation=True,max_length=max_length,return_tensors="pt")
+            print("Input1:")
+            print(inputs)
+            # move tensor value to cuda device  
+            inputs = {k: v.to(target_device) for k, v in inputs.items()}
+            print("Input2:",inputs)
+            # Encode to get hi the representation of ui  
+            outputs = self.model(**inputs, return_dict=True)
+            # the shape of last hidden -> (batch_size, sequence_length, hidden_size)
+            print("outputs:",outputs.last_hidden_state)
+
+            print("outputs:",outputs.last_hidden_state.shape)
+            
+                
---
+++ implement_papers/train.py
@@ -0,0 +1,40 @@
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+import numpy as np
+
+from utils import loss
+from dataloader import IntentExample
+from dataloader import load_intent_examples
+from dataloader import sample
+from dataloader import InputExample
+from dataloader import SimCSE
+from dataloader import Similarity
+
+
+
+# config
+
+train_file_path = '../../dataset/Few-Shot-Intent-Detection/Datasets/CLINC150/train/'
+N = 100  # number of samples per class (100 full-shot)
+T = 1 # number of Trials
+
+
+# 
+train_examples = load_intent_examples(train_file_path)
+"""
+structure of this data  [trials] 
+trail -> [dict1,dict2,...,dict#intents]
+every dict -> {'task':'lable name','examples':[text1,text2,..,textN]}
+"""
+sampled_tasks = [sample(N, train_examples) for i in range(T)]
+
+#print(sampled_tasks[0][0])
+embedding = SimCSE('cuda') 
+
+# Testing fist example 
+sentence = sampled_tasks[0][0]['examples']
+
+
+
+
+embedding.encode(sentence[0])
